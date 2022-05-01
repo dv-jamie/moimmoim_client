7d5fecb258baa9f05878c42e5fbbd3589ac5f45c
dv-jamie <sjm2506@gmail.com>
2022-05-02 03:27:31 +0900

    first commit

--- src/components/LikeIcon.js
+++
@@ -1,9 +0,0 @@
-function LikeIcon({ fill, addLike, deleteLike }) {
-    if(fill) {
-        return <span className="material-icons" onClick={deleteLike}>favorite</span>
-    } else {
-        return <span className="material-icons" onClick={addLike}>favorite_border</span>
-    }
-}
-
-export default LikeIcon;
